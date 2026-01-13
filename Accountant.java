8b231bfbd602a7599804dabf9029ef785bccd64c
Kabir Moulana <kabirmoulana41@gmail.com>
2026-01-13 13:18:01 +0530

    Update Accountant.java

--- Accountant.java
+++ Accountant.java
@@ -1,11 +1,14 @@
-public class Accountant extends Employee {                                       //inheritance
+class Accountant extends Employee                                               //inheritance
+{                                            
 
-    public Accountant(int empId, String name, double basicSalary) {              //constructor
-        super(empId, name, basicSalary);
+    public Accountant(int empId, String name, double basicSalary)              //constructor
+    {
+        super(empId, name, basicSalary);                                       //super calls the parent class constructor
     }
 
-    @Override 
-    public double calculateSalary() {                                            //polymorphism
+    @Override
+    public double calculateSalary()                                            //polymorphism (method over-riding)
+    {
         return basicSalary + 4000;
     }
 }
