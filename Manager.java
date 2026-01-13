0db01d46799245d6226a8ed2c9c4355a2406ee65
Kabir Moulana <kabirmoulana41@gmail.com>
2026-01-13 13:20:27 +0530

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -1,11 +1,14 @@
-public class Manager extends Employee {                                //inheritance
+class Manager extends Employee 
+{                                                                         //inheritance
 
-    public Manager(int empId, String name, double basicSalary) {       //constructor
-        super(empId, name, basicSalary);                               //super calls parentclass constructor
+    public Manager(int empId, String name, double basicSalary)            //constructor
+    {        
+        super(empId, name, basicSalary);                                  //super calss parent class constructor
     }
 
     @Override
-    public double calculateSalary() {
-        return basicSalary + 5000;
+    public double calculateSalary()
+    {                                                                     //polymorphism 
+        return basicSalary + 5000; 
     }
 }
